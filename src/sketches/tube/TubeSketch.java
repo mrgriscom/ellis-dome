b031c2d82283d1b9cc1ce44097168e5f57a3df01
Drew Roos <mrgriscom@gmail.com>
2016-04-23 16:15:51 +0200

    tube parameterization

--- src/sketches/tube/TubeSketch.java
+++ src/sketches/tube/TubeSketch.java
@@ -6,10 +6,15 @@ public class TubeSketch extends PointSampleSketch<PVector, Double> {
     static final double DEFAULT_FOV = 120.;
     static final int DEFAULT_SUBSAMPLING = 4;
 
-    int numCheckers = 4;
-
     double fov;  // Aperture from opposite ends of the display area, in degrees.
     double speed = 1.;
+
+    double v_height = 1.; // Height of an on-off cycle
+    int v_offset = 0;
+    int h_checks = 4;
+    double h_skew = 0;
+    double v_asym = .5;
+    double h_asym = .5;
 
     TubeSketch(PApplet app, double fov, int size_px, int subsampling, boolean temporal_jitter) {
         super(app, size_px, subsampling, temporal_jitter);
@@ -47,8 +52,51 @@ public class TubeSketch extends PointSampleSketch<PVector, Double> {
             });
         ctrl.registerHandler("browse", new InputControl.InputHandler() {
                 public void jog(boolean pressed) {
-                    numCheckers += (pressed ? 1 : -1);
-                    System.out.println(numCheckers);
+                    h_checks += (pressed ? 1 : -1);
+                    System.out.println(h_checks);
+                }
+            });
+        ctrl.registerHandler("jog_b", new InputControl.InputHandler() {
+                public void jog(boolean pressed) {
+                    boolean forward = pressed;
+                    if (Math.abs(h_skew) > .02) {
+                        final double SKEW_INC = 1.01;
+                        h_skew *= (forward == h_skew > 0 ? SKEW_INC : 1./SKEW_INC);
+                    } else {
+                        final double SKEW_STEP = .001;
+                        h_skew += (forward ? 1 : -1) * SKEW_STEP;
+                    }
+                }
+            });
+        ctrl.registerHandler("pitch_a", new InputControl.InputHandler() {
+                public void slider(double val) {
+                    h_asym = val;
+                }
+            });
+        ctrl.registerHandler("pitch_b", new InputControl.InputHandler() {
+                public void slider(double val) {
+                    v_asym = val;
+                }
+            });
+        ctrl.registerHandler("pitch_inc_a", new InputControl.InputHandler() {
+                public void button(boolean pressed) {
+                    if (pressed) {
+                        v_offset += 1;
+                    }
+                }
+            });
+        ctrl.registerHandler("pitch_dec_a", new InputControl.InputHandler() {
+                public void button(boolean pressed) {
+                    if (pressed) {
+                        v_offset -= 1;
+                    }
+                }
+            });
+        ctrl.registerHandler("mixer", new InputControl.InputHandler() {
+                public void slider(double val) {
+                    double HMIN = .2;
+                    double HMAX = 8.;
+                    v_height = HMIN * Math.pow(HMAX / HMIN, val);
                 }
             });
     }
@@ -76,12 +124,17 @@ public class TubeSketch extends PointSampleSketch<PVector, Double> {
         double pos0 = state;
         double pos = pos0 + speed * t_jitter;
 
-        double u_pct = MathUtil.fmod(uv.x / (2*Math.PI), 1.);
+        double u_unit = MathUtil.fmod(uv.x / (2*Math.PI), 1.);
         double dist = uv.y + pos;
-        //boolean chk = ((int)MathUtil.fmod((uv.x + dist)/(.25*Math.PI), 2.) + (int)MathUtil.fmod((uv.x - dist)/(.25*Math.PI), 2.)) % 2 == 0;
-        boolean chk = ((int)MathUtil.fmod((uv.x)/(Math.PI/numCheckers), 2.) + (int)MathUtil.fmod((dist)/(Math.PI/numCheckers), 2.)) % 2 == 0;
-        //boolean chk = (MathUtil.fmod((uv.x + dist) / Math.PI, 2.) < 1.);
-        return color(MathUtil.fmod(u_pct + dist/10., 1.), .5, chk ? 1 : .05);
+
+        return checker(dist, u_unit);
+    }
+
+    int checker(double dist, double u_unit) {
+        boolean v_on = (v_height > 0 ? MathUtil.fmod(dist / v_height - v_offset * u_unit, 1.) < v_asym : false);
+        boolean u_on = (MathUtil.fmod((u_unit + h_skew * dist) * h_checks, 1.) < h_asym);
+        boolean chk = u_on ^ v_on;
+        return color(MathUtil.fmod(u_unit + dist/10., 1.), .5, chk ? 1 : .05);
     }
 
 }
