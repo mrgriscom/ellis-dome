ef38f6bb5f24505121447627cc49b4275c77cf07
Drew Roos <mrgriscom@gmail.com>
2018-05-15 20:00:43 +0200

    move game launch code into core library

--- src/scripts/play_emulator.py
+++ src/scripts/play_emulator.py
@@ -7,25 +7,8 @@ import launch
 
 #sudo apt-get install retroarch libretro-bsnes-mercury-performance libretro-gambatte libretro-nestopia libretro-genesisplusgx
 
-roms_path = '/home/drew/roms/'
-cores = {
-    'gameboy': '/usr/lib/libretro/gambatte_libretro.so',
-    'nes': '/usr/lib/libretro/nestopia_libretro.so',
-    'snes': '/usr/lib/libretro/bsnes_mercury_performance_libretro.so',
-    'genesis': '/usr/lib/libretro/genesis_plus_gx_libretro.so',
-}
-
 rom = sys.argv[1]
-rompath = os.path.abspath(rom)
-core = None
-for k, v in cores.iteritems():
-    if rompath.startswith(os.path.join(roms_path, k)):
-        core = v
-        break
-if not core:
-    raise RuntimeError('don\'t recognize system')
-
-_, procs = launch.launch_screencast('retroarch -L %s "%s"' % (core, rom), {'title': 'retroarch'})
+_, procs = launch.launch_emulator(rom)
 try:
     while True:
         time.sleep(.01)
